1d793f59a8659aa254d336ba87dac6212fcbe087
Ivan Vasilevich <ivanvbesarab@gmail.com>
2017-02-14 19:40:45 +0200

    poehali

---
+++ G51L7/TutorialVC.swift
@@ -0,0 +1,21 @@
+//
+//  TutorialVC.swift
+//  G51L7
+//
+//  Created by Ivan Vasilevich on 2/14/17.
+//  Copyright © 2017 Smoosh Labs. All rights reserved.
+//
+
+import UIKit
+
+class TutorialVC: UIViewController {
+
+    
+    @IBAction func poehaliPressed() {
+        dismiss(animated: true) { 
+            print("kozhanuy appears")
+        }
+    }
+    
+    
+}
